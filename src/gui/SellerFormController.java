3a48491073c735fcc3ff7a90caa2a930cc8feeab
Marcos Sousa <sousa.marcoscv@gmail.com>
2021-05-25 21:31:29 -0300

    SellerForm

---
+++ src/gui/SellerFormController.java
@@ -0,0 +1,138 @@
+package gui;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+import db.DbException;
+import gui.listeners.DataChangeListener;
+import gui.util.Alerts;
+import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import model.entities.Seller;
+import model.exception.ValidationException;
+import model.services.SellerService;
+
+public class SellerFormController implements Initializable {
+
+	private Seller entity;
+
+	private SellerService service;
+
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
+
+	@FXML
+	private TextField txtId;
+
+	@FXML
+	private TextField txtname;
+
+	@FXML
+	private Label labelErrorName;
+
+	@FXML
+	private Button btSave;
+
+	@FXML
+	private Button btCancel;
+
+	public void setSeller(Seller entity) {
+		this.entity = entity;
+	}
+
+	public void setSellerService(SellerService service) {
+		this.service = service;
+	}
+
+	public void subscribeDataChangeListener(DataChangeListener listener) {
+		dataChangeListeners.add(listener);
+	}
+
+	@FXML
+	public void onBtSaveAction(ActionEvent event) {
+		if (entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+
+		if (service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+
+		try {
+			entity = getFormData();
+			service.SaveOrUptade(entity);
+			notifyDataChangeListeners();
+			Utils.currentStage(event).close();
+		} catch (DbException e) {
+			Alerts.showAlert("Error save object", null, e.getMessage(), AlertType.ERROR);
+		} catch (ValidationException e) {
+			setErrorMessages(e.getErrors());
+		}
+	}
+
+	private void notifyDataChangeListeners() {
+		for (DataChangeListener listeners : dataChangeListeners) {
+			listeners.onDataChanged();
+		}
+
+	}
+
+	private Seller getFormData() {
+		Seller obj = new Seller();
+
+		ValidationException exception = new ValidationException("Validation error");
+
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+
+		if (txtname.getText() == null || txtname.getText().trim().equals("")) {
+			exception.addError("name", "Field cant't be empty");
+		}
+		obj.setName(txtname.getText());
+
+		if (exception.getErrors().size() > 0) {
+			throw exception;
+		}
+		return obj;
+	}
+
+	@FXML
+	public void onBtCancelAction(ActionEvent event) {
+		Utils.currentStage(event).close();
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		initializeNodes();
+
+	}
+
+	private void initializeNodes() {
+		Constraints.setTextFieldInteger(txtId);
+		Constraints.setTextFieldMaxLength(txtname, 30);
+	}
+
+	public void updateFormData() {
+		if (entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		txtId.setText(String.valueOf(entity.getId()));
+		txtname.setText(entity.getName());
+	}
+
+	private void setErrorMessages(Map<String, String> errors) {
+		Set<String> fields = errors.keySet();
+		if (fields.contains("name")) {
+			labelErrorName.setText(errors.get("name"));
+		}
+	}
+}
